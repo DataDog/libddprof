7ead908d6bd2f5604bede7234c8d605559c9e89c
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-02-01 14:59:27 +0000

    Bump gem version to beta2

--- ruby/lib/libddprof/version.rb
+++ ruby/lib/libddprof/version.rb
@@ -5,5 +5,5 @@ module Libddprof
   LIB_VERSION = "0.2.0"
 
   # This is the Ruby gem version -- often it will match LIB_VERSION, but sometimes it may not
-  VERSION = "#{LIB_VERSION}.beta1"
+  VERSION = "#{LIB_VERSION}.beta2"
 end
