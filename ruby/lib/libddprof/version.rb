0ea7bc476cec6f3c73ebc62d21bcfce9d78e99ba
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-02-02 16:23:26 +0000

    Adopt new versioning scheme, as suggested during PR review
    Thanks @lloeki for the idea :)

--- ruby/lib/libddprof/version.rb
+++ ruby/lib/libddprof/version.rb
@@ -4,6 +4,14 @@ module Libddprof
   # Current libddprof version
   LIB_VERSION = "0.2.0"
 
-  # This is the Ruby gem version -- often it will match LIB_VERSION, but sometimes it may not
-  VERSION = "#{LIB_VERSION}.beta2"
+  GEM_MAJOR_VERSION = "1"
+  GEM_MINOR_VERSION = "0"
+  GEM_PRERELEASE_VERSION = ".beta3"
+  private_constant :GEM_MAJOR_VERSION, :GEM_MINOR_VERSION, :GEM_PRERELEASE_VERSION
+
+  # The gem version scheme is lib_version.gem_major.gem_minor[.prerelease].
+  # This allows a version constraint such as ~> 0.2.0.1.0 in the consumer (ddtrace), in essence pinning the libddprof to
+  # a specific version like = 0.2.0, but still allow a) introduction of a gem-level breaking change by bumping gem_major
+  # and b) allow to push automatically picked up bugfixes by bumping gem_minor.
+  VERSION = "#{LIB_VERSION}.#{GEM_MAJOR_VERSION}.#{GEM_MINOR_VERSION}#{GEM_PRERELEASE_VERSION}"
 end
