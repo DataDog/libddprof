91ae4ad684ca5efad020a4254966b000d74b33a8
Levi Morrison <levi.morrison@datadoghq.com>
2022-04-13 08:58:38 -0600

    Add license info to files

--- ddprof-ffi/src/tags.rs
+++ ddprof-ffi/src/tags.rs
@@ -1,3 +1,6 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022-Present Datadog, Inc.
+
 use crate::{AsBytes, CharSlice};
 use ddprof_exporter::parse_tags;
 use ddprof_exporter::tag::Tag;
